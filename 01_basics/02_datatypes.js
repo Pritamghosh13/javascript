e71ff474cd1aebc22e41699886fc7f78bc41e8ad
Pritamghosh13 <ghoshpritam0708@gmail.com>
2025-12-27 20:23:15 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"  //use all js code as a new version
+
+//nodealert(3+3)   //we are using note.js not browser
+
+let name = "Pritam"
+let age = 19
+let isLoggedIn = true
+
+//all kinds of datatypes
+//numbers
+//bigint
+//string
+//boolean = true/false
+//null => standalone value (object)
+//undefiend =>
+//symbol => unique
+
+//object
+
+console.log(typeof "Pritam")
+console.log(typeof age)
+console.log(typeof isLoggedIn)
+console.log(typeof null)
+console.log(typeof undefined)
