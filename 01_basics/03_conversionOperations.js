d47d5d46d51532621640032b9c7b7da8bf2af751
Pritam Ghosh <ghoshpritam0708@gmail.com>
2025-12-29 17:47:43 +0000

    conversion and Operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -35,8 +35,8 @@ console.log(booleanLoggedIn);
 
 
 
-/*
 
+/*
 number to string:
 33 => "33"
 
@@ -52,6 +52,55 @@ number to boolean:
 string to boolean:
 "" => false   (Empty string)
 "Pritam" => true (Normal string)
-
 */
+
+
+
+// *********************************Operations**********************************
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+
+console.log(2+3);
+console.log(3-2);
+console.log(3*2);
+console.log(4**3);     //maens 4 to the power of 3
+console.log(2%3);
+console.log(3/2);
+
+
+let str1 = "Pritam"
+let str2 = " Ghosh"         //adition of two strings
+let str3 = str1 + str2
+console.log(str3);
+
+
+//important operations
+console.log("1" + 2);
+console.log(1 + "2");
+console.log(1 + 2 + "3");
+console.log("1" + 2 + 3);
+
+
+
+console.log(true);       //output: true
+
+console.log(+true);      //output: 1
+
+console.log(false);      //output: false
+
+console.log(+false);     //output: 0
+
+
+
+let num = 100
+console.log(++num);     
+console.log(num++);
+
+
+
+
+
 
