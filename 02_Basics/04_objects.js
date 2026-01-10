e6d58b7a94b6bdc0c8daee2a50748e4e25c1a842
Pritam Ghosh <ghoshpritam0708@gmail.com>
2026-01-10 23:48:04 +0530

    Update 04_objects.js

--- 02_Basics/04_objects.js
+++ 02_Basics/04_objects.js
@@ -6,7 +6,7 @@ tinderUser.id = "123abc"
 tinderUser.name = "Pritam"
 tinderUser.isLoggedIn = false
 
-// console.log(tinderUser);
+console.log(tinderUser);
 
 const regularUser = {
     email: "ghosh@gmail.com",
@@ -18,22 +18,22 @@ const regularUser = {
     }
 }
 
-// console.log(regularUser.fullname.userfullname);
+console.log(regularUser.fullname.userfullname);
 
 const obj1 = {1: "a", 2: "b"}
 const obj2 = {3: "a", 4: "b"}
 const obj3 = {5: "a", 6: "b"}
 
 Object.assign(obj1, obj2, obj3)    //add the all object together, stores in "obj1"
-// console.log(obj1);
+console.log(obj1);
 
-// const obj4 = Object.assign({}, obj1, obj2, obj3);  //add the all object together, stores in "obj4"
-// console.log(obj4);
+const obj4 = Object.assign({}, obj1, obj2, obj3);  //add the all object together, stores in "obj4"
+console.log(obj4);
 
-// console.log(obj1 === obj4); //gives output false. because the merge object stores in the "{}"
+console.log(obj1 === obj4); //gives output false. because the merge object stores in the "{}"
 
 const obj5 = {...obj1, ...obj2, ...obj3} //most efficient process to add to merge two object
-// console.log(obj5);
+console.log(obj5);
 
 const users = [
     {
@@ -56,16 +56,16 @@ const users = [
 ]
 
 
-// console.log(users[1].email);
-// console.log(tinderUser);
-// console.log(Object.keys(tinderUser));  //make a array of "keys" of the object
-// console.log(Object.values(tinderUser));  //make a array of "Values" of the object
-// console.log(Object.entries(tinderUser));  //make array of array , which stores the keys and values of the object
+console.log(users[1].email);
+console.log(tinderUser);
+console.log(Object.keys(tinderUser));  //make a array of "keys" of the object
+console.log(Object.values(tinderUser));  //make a array of "Values" of the object
+console.log(Object.entries(tinderUser));  //make array of array , which stores the keys and values of the object
 
-// console.log(tinderUser.hasOwnProperty('isLoggedIn'));  //search that the given data present or not, give output in boolean.
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));  //search that the given data present or not, give output in boolean.
 
 
-// console.log(Object.values(regularUser));  
+console.log(Object.values(regularUser));  
 
 
 const course = {
@@ -79,3 +79,4 @@ console.log(course.courseInstractor);
 
 const {courseInstractor : Instractor} = course;  //Now we print the Instractor that print the object value of courseInstractor.
 console.log(Instractor);
+
